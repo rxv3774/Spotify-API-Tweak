3afb105a772502d539f10e511b455084c6a2ae58
Ryan Vasquez <rxv3774@rit.edu>
2018-12-28 20:13:19 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -2,26 +2,57 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let textColor = '#fff';
+let setStyle = {color: textColor};
 class App extends Component {
   render() {
+    let name = 'Ryan'
+    let setColor = '#FF2212'
+    let headerStyle = {...setStyle, 'fontsize': '40px'}
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Title</h1>
+        <Aggregate/>
+        <Aggregate/>
+        <Filter/>
+        <Playlist/>
+        <Playlist/>
+        <Playlist/>
       </div>
     );
+  }
+}
+
+class Aggregate extends Component {
+  render() {
+    return (
+      <div style={{width: "50%", display: 'inline-block'}}>
+        <h2 style = {setStyle}> Number Text</h2>
+      </div>
+    )
+  }
+}
+
+class Filter extends Component {
+  render() {
+    return(
+      <div style={setStyle}>
+        <img/>
+        <input type="text"/>
+      </div>
+    )
+  }
+}
+
+class Playlist extends Component {
+  render() {
+    return (
+      <div style={{...setStyle, width: '30%', display: 'inline-block'}}>
+        <img />
+        <h3>Playlist Name</h3>
+        <ul><li>Song 1</li><li>Song 2</li><li>Song 3</li></ul>
+      </div>
+    )
   }
 }
 
